5c23b0d4428c307b5f25df1591547d2e04d873cf
JorgeLg3 <jorgelopezgamarra@gmail.com>
2022-04-25 15:04:39 +0200

    Score display, and start button

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,9 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
 import game_functions as gf
 
 def run_game():
@@ -14,6 +17,11 @@ def run_game():
             (ai_settings.screen_width, ai_settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        play_button = Button(ai_settings, screen, "Play")
+
+        stats = GameStats(ai_settings)
+        sb = Scoreboard(ai_settings, screen, stats)
+
         ship = Ship(ai_settings, screen)
 
         bullets = Group()
@@ -22,10 +30,12 @@ def run_game():
         gf.create_fleet(ai_settings, screen, ship,  aliens)
 
         while True:
-            gf.check_events(ai_settings, screen, ship, bullets)
-            ship.update()
-            gf.update_bullets(aliens, bullets)
-            gf.update_aliens(ai_settings, aliens)
-            gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+            gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
+
+            if stats.game_active:
+                ship.update()
+                gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+                gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 run_game()
